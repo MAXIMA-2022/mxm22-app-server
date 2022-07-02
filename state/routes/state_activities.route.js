f097b9b89d06dfd81c3745c5506b3c2d0e4d50f6
Felix Ferdianto <77229194+xileferdianto08@users.noreply.github.com>
2022-07-02 23:49:51 +0700

    feat: middleware isPanitia for state_activities
    create data state_act = isPanitia
    read data state_act=  all (login first)
    update data state_act = isPanitia
    delete data state_act = isPanitia --> Organisator account also deleted (for this we just use ON DELETE CASCADE from database)

--- state/routes/state_activities.route.js
+++ state/routes/state_activities.route.js
@@ -1,34 +1,36 @@
 const sActController = require('../controllers/state_activties.controller')
-const authJWTMiddleware = require ('../../user/middleware/user.middleware')
+const middleware = require ('../../user/middleware/user.middleware')
 const validation = require ('../validation/validate')
 
 module.exports = function(app){
-    app.post(
-        '/api/state/createState',
-        authJWTMiddleware.verifyJWT, validation.createStateActValidation, validation.runValidation,
-        sActController.createState
-    )
-
     app.get(
         '/api/state',
-        authJWTMiddleware.verifyJWT,
+        middleware.verifyJWT, 
         sActController.readAllState
     )
     app.get(
         '/api/state/:stateID',
-        authJWTMiddleware.verifyJWT,
+        middleware.verifyJWT,
         sActController.readSpecificState
+    )
+
+    app.post(
+        '/api/state/createState',
+        middleware.verifyJWT, middleware.isPanitia, 
+        validation.createStateActValidation, validation.runValidation,
+        sActController.createState
     )
 
     app.put(
         '/api/state/update/:stateID',
-        authJWTMiddleware.verifyJWT, validation.updateStateActValidation, validation.runValidation,
+        middleware.verifyJWT, middleware.isPanitia,
+        validation.updateStateActValidation, validation.runValidation,
         sActController.updateState
     )
 
     app.delete(
         '/api/state/delete/:stateID',
-        authJWTMiddleware.verifyJWT,
+        middleware.verifyJWT, middleware.isPanitia,
         sActController.deleteState
     )
 }

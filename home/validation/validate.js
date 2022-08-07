797732c91cb5b89b0af8510be9c06e194bd72c25
Felix Ferdianto <77229194+xileferdianto08@users.noreply.github.com>
2022-08-07 17:02:44 +0700

    fix: API update bugs on state_act & home_info

--- home/validation/validate.js
+++ home/validation/validate.js
@@ -18,9 +18,9 @@ exports.createHMediaValidation = [
     check('homeID').notEmpty().withMessage('HoME ID tidak boleh kosong')
 ]
 
-exports.updateHMediaValidation = [
-    check('photoID').notEmpty().withMessage('Photo ID tidak boleh kosong')
-]
+// exports.updateHMediaValidation = [
+//     check('photoID').notEmpty().withMessage('Photo ID tidak boleh kosong')
+// ]
 
 
 exports.insertLogoValidation = (req, res, next) => {
@@ -99,13 +99,6 @@ exports.updateLogoValidation = (req, res, next) => {
 
     let isAccepted = ''
 
-    if (!req.files || !req.files.linkLogo) {
-        fileErrors.push({
-            key: 'linkLogo',
-            message: 'Gambar Logo tidak boleh kosong'
-        })
-    }
-    
     if (req.files) 
         isAccepted = acceptedType.includes(req.files.linkLogo.mimetype)
 
@@ -121,45 +114,45 @@ exports.updateLogoValidation = (req, res, next) => {
 }
 
 
-exports.updateMediaValidation = (req, res, next) => {
-    const mediaErrors = []
-    const acceptedType = ['image/png', 'image/jpg', 'image/jpeg']
+// exports.updateMediaValidation = (req, res, next) => {
+//     const mediaErrors = []
+//     const acceptedType = ['image/png', 'image/jpg', 'image/jpeg']
 
-    let linkMedia = []
+//     let linkMedia = []
 
-    switch (true) {
-        case !req.files :
-            mediaErrors.push({
-                key: 'linkMedia',
-                message: 'Media Tidak Boleh Kosong'
-            })
-            break
-        // case req.files.linkMedia.length !== req.body.photoID.length :
-        //     mediaErrors.push({
-        //         key: 'linkMedia',
-        //         message: 'Terdapat salah satu form Media yang kosong'
-        //     })
-        //     break
-        case req.files.linkMedia.length === undefined :
-            linkMedia = [req.files.linkMedia]
-            break
-        case req.files.linkMedia.length !== undefined :
-            linkMedia = req.files.linkMedia
-            break
-    }
+//     switch (true) {
+//         case !req.files :
+//             mediaErrors.push({
+//                 key: 'linkMedia',
+//                 message: 'Media Tidak Boleh Kosong'
+//             })
+//             break
+//         // case req.files.linkMedia.length !== req.body.photoID.length :
+//         //     mediaErrors.push({
+//         //         key: 'linkMedia',
+//         //         message: 'Terdapat salah satu form Media yang kosong'
+//         //     })
+//         //     break
+//         case req.files.linkMedia.length === undefined :
+//             linkMedia = [req.files.linkMedia]
+//             break
+//         case req.files.linkMedia.length !== undefined :
+//             linkMedia = req.files.linkMedia
+//             break
+//     }
 
-    for (let i = 0; i < linkMedia.length; i++) {
-        if (!acceptedType.includes(linkMedia[i].mimetype)) {
-            mediaErrors.push({
-                key: `linkMedia-${i + 1}`,
-                message: 'Gambar Media harap menggunakan file png, jpg, atau jpeg'
-            })
-        }
-    }
+//     for (let i = 0; i < linkMedia.length; i++) {
+//         if (!acceptedType.includes(linkMedia[i].mimetype)) {
+//             mediaErrors.push({
+//                 key: `linkMedia-${i + 1}`,
+//                 message: 'Gambar Media harap menggunakan file png, jpg, atau jpeg'
+//             })
+//         }
+//     }
 
-    req.mediaErrors = mediaErrors
-    next()
-}
+//     req.mediaErrors = mediaErrors
+//     next()
+// }
 
 
 exports.runValidation = (req, res, next) => {
